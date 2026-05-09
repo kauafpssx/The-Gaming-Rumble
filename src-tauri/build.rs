32d5e0361e624a06b584ae10d76c8c2aae398e37
zKauaFerreira <kaua@agencyos.com>
2026-05-08 21:25:39 -0300

    fix: revert broken admin manifest in v1.0.6

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,3 @@
 fn main() {
-    let windows = tauri_build::WindowsAttributes::new().app_manifest(include_str!("admin-manifest.xml"));
-    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
-    tauri_build::try_build(attributes).expect("failed to run tauri build script");
+    tauri_build::build()
 }
